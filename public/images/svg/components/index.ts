99d2af26ab0bbb20b0279aef9785b913a54551a7
Felipe Medina <felipe.medina@samgreencorp.com>
2023-10-23 23:45:06 -0500

    fix_error_build

--- public/images/svg/components/index.ts
+++ public/images/svg/components/index.ts
@@ -1,4 +1,1 @@
 export { CheckWhiteIcon } from "./Check";
-export { FacebookIcon } from "./FacebookIcon";
-export { InstagramIcon } from "./InstagramIcon";
-export { TwitterIcon } from "./TwitterIcon";
